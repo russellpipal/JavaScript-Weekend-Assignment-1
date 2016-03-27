edc0ba9f4c39bafeea100d06b14f53f04a507bc3
russellpipal <russell.pipal@gmail.com>
2016-03-27 00:41:34 -0500

    Fixes small typos

--- app.js
+++ app.js
@@ -2,44 +2,44 @@ var claim1 = {
 	patientName: 'John Doe',
 	visitType: 'Specialist',
 	visitCost: 1100
-}
+};
 
 var claim2 = {
 	patientName: 'Jane Doe',
 	visitType: 'Optical',
 	visitCost: 100
-}
+};
 
 var claim3 = {
 	patientName: 'Joe Johnson',
 	visitType: 'Emergency',
 	visitCost: 31000
-}
+};
 
 var claim4 = {
 	patientName: 'Sharon Smith',
 	visitType: 'Emergency',
 	visitCost: 1300
-}
+};
 
 var claim5 = {
 	patientName: 'Steve Wright',
 	visitType: 'Primary Care',
 	visitCost: 770
-}
+};
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
+var initialList = [claim1, claim2, claim3, claim4, claim5];
 
-var totalPayedOut = 0;
+var totalPaidOut = 0;
 
-function Claim(name, type, cost){
+function Claim(name, type, cost) {
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
 }
 
 //adds new claim to list
-function addClaim(list, name, type, cost){
+function addClaim(list, name, type, cost) {
 	var newClaim = new Claim(name, type, cost);
 	return list.push(newClaim);
 }
@@ -52,8 +52,8 @@ addClaim(initialList, 'Flint Ironstag', 'Primary Care', 22000);
 addClaim(initialList, 'Mitchell', 'Emergency', 200000);
 
 // determine percentage of claim covered
-function claimPercent(aClaim){
-	switch (aClaim.visitType){
+function claimPercent(aClaim) {
+	switch (aClaim.visitType) {
 		case 'Optical':
 			return 0;
 		case 'Specialist':
@@ -68,24 +68,24 @@ function claimPercent(aClaim){
 }
 
 //processes claims list
-function processClaims(claims){
+function processClaims(claims) {
 	var amtPaid = 0;
 	var message = '';
 	var jMessage = '';
-	for ( var i=0; i<claims.length; i++ ){
+	for ( var i=0; i<claims.length; i++ ) {
 		amtPaid = Math.round(claimPercent(claims[i]) * claims[i].visitCost);
 		message = 'Paid out $' + amtPaid.toLocaleString() + ' for ' + claims[i].patientName;
 		console.log(message);
-		jMessage = '<p>' + message + '</p>'
+		jMessage = '<p>' + message + '</p>';
 		$('main').append(jMessage);
-		totalPayedOut += amtPaid;
+		totalPaidOut += amtPaid;
 	}
 }
 
 $(document).ready(function(){
 	processClaims(initialList);
-	var totalMessage = 'Total amount paid: $' + totalPayedOut.toLocaleString()
+	var totalMessage = 'Total amount paid: $' + totalPaidOut.toLocaleString();
 	console.log(totalMessage);
 	var jTotalMessage = '<h3>' + totalMessage + '</h3>';
 	$('main').append(jTotalMessage);
-})
+});
